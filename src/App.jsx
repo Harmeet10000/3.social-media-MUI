3ee5276f6b18c114fd021815d82100d3abb979b4
Harmeet10000 <harmeetsinghfbd@gmail.com>
2024-02-15 10:35:49 +0530

    Front-end with GUI

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import Rightbar from "./components/Rightbar";
+import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
+import { Box, Stack, ThemeProvider, createTheme } from "@mui/material";
+import Navbar from "./components/Navbar";
+import Add from "./components/Add.jsx";
+import { useState } from "react";
+
+function App() {
+  const [mode, setMode] = useState("dark");
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
+        <Navbar />
+        <Stack direction={"row"} spacing={2} justifyContent={"space-between"}>
+          <Sidebar setMode={setMode} mode={mode}></Sidebar>
+          <Feed></Feed>
+          <Rightbar></Rightbar>
+        </Stack>
+        <Add />
+      </Box>
+    </ThemeProvider>
+  );
+}
+
+export default App;
